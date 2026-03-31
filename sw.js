00f90cc57976c0ec1ed175b6b76dbf99a11850c4
ExecuteMyExecution <ExecuteMyExecution@users.noreply.github.com>
2026-03-31 13:16:06 +0000

    deploy: ExecuteMyExecution/Blog-Hexo@abd7e382db695a6d7a7148e8f7bb73e31b635c15

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "nocturne-tome-v1";
+const CACHE_URLS = ["/", "/css/app.css"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CACHE_URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(
+        names
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response.ok) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
